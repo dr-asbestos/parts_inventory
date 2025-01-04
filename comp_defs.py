c6e86bfa8cfcf4c3ba8ce30d176722a718f0e172
dr-asbestos <egor.khramenko@mail.ru>
2025-01-04 23:14:19 +1300

    basic idea

--- comp_defs.py
+++ comp_defs.py
@@ -5,29 +5,33 @@ common inherited properties from each other.
 
 class Component:
     '''
-    Generic component
+    Generic component definition
     '''
-    def __init__(self, *, 
-                 id=None, 
-                 package=None, 
-                 description=None):
-        self.id = id
-        self.package = package
-        self.description = description
+    def __init__(self):
+        self.id = None
+        self.package = None
+        self.description = None
+
+    def set_field(self, field, value):
+        '''Dedicated field setter method. Avoid setting fields manually.'''
+        if hasattr(self, field):
+            setattr(self, field, value)
+        else:
+            raise AttributeError(f"{self.__class__.__name__} component has no field '{field}'.")
 
 class Resistor(Component):
     '''
     Resistor definition
     '''
-    def __init__(self, *, 
-                 id=None, 
-                 package=None, 
-                 description=None, 
-                 value=None, 
-                 power=None):
-        super.__init__(id=id, 
-                 package=package, 
-                 description=description)
-        self.value = value
-        self.power = power
+    def __init__(self):
+        super().__init__()
+        self.value = None
+        self.power = None
 
+
+
+# testing bits on the go
+test = Resistor()
+test.set_field('value', 123)
+print(test.value)
+test.set_field('asdf', 1234)
