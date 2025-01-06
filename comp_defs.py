6cb4c2c94418397f80586f98562cfc68ee63dc6c
dr-asbestos <egor.khramenko@mail.ru>
2025-01-06 18:33:52 +1300

    trying out something wacky with slots

--- comp_defs.py
+++ comp_defs.py
@@ -5,25 +5,29 @@ common inherited properties from each other.
 
 class Component:
     '''Generic component definition and functions.'''
+    __slots__ = ('id', 'mount', 'desc')
     def __init__(self):
-        self.id = None
-        self.mount = None
-        self.desc = None
+        '''Dynamically create all class attributes/fields from __slots__.'''
+        for slots in (getattr(cls, '__slots__', ()) for cls in self.__class__.__mro__):
+            for slot in slots:
+                setattr(self, slot, None)
 
-    def set_field(self, field, value):
-        '''Dedicated field setter method. Avoid setting fields manually.'''
-        if hasattr(self, field):
+    def set_fields(self, fields):
+        '''Takes a dictionary of fields and values, and sets this component's 
+        fields and values. Raises AttribureError in case of failure.'''
+        for field, value in fields.items():
             setattr(self, field, value)
-        else:
-            raise AttributeError(f"{self.__class__.__name__} component has no field '{field}'.")
     
     def get_all_fields(self):
-        '''Return a dictionary of all fields assossiated with this component.'''
-        return vars(self)
+        '''Return a list of all fields assossiated with this component.'''
+        slots = []
+        for cls in self.__class__.__mro__:
+            slots += getattr(cls, '__slots__', [])
+        return slots
     
     def get_empty_fields(self):
         '''Return a list of all fields that are None.'''
-        return [field for field, value in self.get_all_fields().items() if value is None]
+        return [field for field in self.get_all_fields() if getattr(self, field, None) is None]
 
     def get_inheritance(self, raw=False):
         '''Returns the inheritance chain list  of the component in form of 
@@ -32,65 +36,70 @@ class Component:
             return list(self.__class__.__mro__)
         else:
             return [cls.__name__ for cls in self.__class__.__mro__]
-        
+    
 class Passive(Component):
+    __slots__ = ('value', 'type')
     def __init__(self):
         super().__init__()
-        self.value = None
-        self.type = None
 
 class Resistor(Passive):
+    __slots__ = ('power',)
     def __init__(self):
         super().__init__()
-        self.power = None
 
 class Capacitor(Passive):
+    __slots__ = ('voltage',)
     def __init__(self):
         super().__init()
-        self.voltage = None
 
 class Inductor(Passive):
+    __slots__ = ('geometry', 'current')
     def __init__(self):
         super().__init__()
-        self.geometry = None
-        self.current = None
 
 class IntegratedCircuit(Component):
+    __slots__ = ('number',)
     def __init__(self):
         super().__init__()
-        self.number = None
-        pass #tbd
 
 class Analog(IntegratedCircuit):
+    __slots__ = ()
     def __init__(self):
         super().__init__()
-        pass #tbd
 
 class Digital(IntegratedCircuit):
+    __slots__ = ('family',)
     def __init__(self):
         super().__init__()
-        self.family = None
-        pass #tbd
 
 class Logic(Digital):
+    __slots__ = ()
     def __init__(self):
         super().__init__()
-        pass #tbd
 
 class Regulator(IntegratedCircuit):
+    __slots__ = ('voltage', 'current')
     def __init__(self):
         super().__init__()
-        self.voltage = None
-        self.current = None
-        pass #tbd
 
 
 
 # testing bits on the go
 test = Resistor()
-test.set_field('value', 123)
-test.set_field('power', 0.25)
+test.set_fields({'value': 123, 'power': 0.25})
+test.id = 999
+
 print(test.get_all_fields())
 print(test.get_empty_fields())
 print(test.get_inheritance())
 print(test.get_inheritance(raw=True))
+
+try:
+    test.set_fields({'haha': 777})
+except Exception as e:
+    print(e)
+
+try:
+    test.haha = 777
+except Exception as e:
+    print(e)
